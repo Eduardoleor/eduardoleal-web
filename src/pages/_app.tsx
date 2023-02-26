74446847d4e4d31fbef57c122719d598f059a8d0
eduardo.leal <eduardo.leal.or@gmail.com>
2023-02-26 03:06:55 -0600

    feature: implement sort imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
-import type { AppProps } from "next/app";
 import { CacheProvider, EmotionCache } from "@emotion/react";
-import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import { createTheme,CssBaseline, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+
+import createEmotionCache from "../hooks/createEmotionCache";
+import lightThemeOptions from "../theme/lightThemeOptions";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-
-import createEmotionCache from "../hooks/createEmotionCache";
-import lightThemeOptions from "../theme/lightThemeOptions";
-
 import "@/styles/globals.css";
 
 interface MyAppProps extends AppProps {
